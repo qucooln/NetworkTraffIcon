e898baf589e8327b053343d4d32c13ebbf4af0c4
Qucooln <qucooln@Qucoolns-MacBook-Pro.local>
2012-05-23 19:57:36 +0800

    Initial Commit

---
+++ NetworkTraffIcon/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  NetworkTraffIcon
+//
+//  Created by Qucooln on 5/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
